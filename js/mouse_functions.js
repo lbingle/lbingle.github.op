43e836cabe122078ef7edce5e5ed389e44f55712
lbingle <lebingle@gmail.com>
2017-11-26 11:03:10 -0800

    javascript

---
+++ js/mouse_functions.js
@@ -0,0 +1,60 @@
+//mouseover and mouseoff functions
+		function bigMOA(x) {
+			x.style.height = "18vh";
+			x.style.width = "9.6vw";
+		}
+
+		function normalMOA(x) {
+			x.style.height = "15vh";
+			x.style.width = "8vw";
+		}
+
+		function bigNitobe(x) {
+			x.style.height = "25.2vh";
+			x.style.width = "13.2vw";
+		}
+
+		function normalNitobe(x) {
+			x.style.height = "20.5vh";
+			x.style.width = "11vw";
+		}
+
+		function bigBeatty(x) {
+			x.style.height = "25.2vh";
+			x.style.width = "12vw";
+		}
+
+		function normalBeatty(x) {
+			x.style.height = "20.5vh";
+			x.style.width = "10vw";
+		}
+
+		function bigNest(x) {
+			x.style.height = "18vh";
+			x.style.width = "10.08vw";
+		}
+
+		function normalNest(x) {
+			x.style.height = "16vh";
+			x.style.width = "8.4vw";
+		}
+
+		function bigPacific(x) {
+			x.style.height = "13.2vh";
+			x.style.width = "7.2vw";
+		}
+
+		function normalPacific(x) {
+			x.style.height = "11vh";
+			x.style.width = "6vw";
+		}
+
+		function bigBus(x) {
+			x.style.height = "14.4vh";
+			x.style.width = "13.2vw";
+		}
+
+		function normalBus(x) {
+			x.style.height = "12vh";
+			x.style.width = "11vw";
+		}
