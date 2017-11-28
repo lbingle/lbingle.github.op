9d35a475f7adf3778bef3fd86fa9cd5b9a5430aa
lbingle <lebingle@gmail.com>
2017-11-27 21:24:19 -0800

    Display locations on mouse over buttons

--- js/mouse_functions.js
+++ js/mouse_functions.js
@@ -1,72 +1,10 @@
 //mouseover and mouseoff functions
-		function bigMOA() {
-			var x = document.getElementById("MOA");
-			x.style.height = "18vh";
-			x.style.width = "9.6vw";
+		function showStuff(id) {
+			var x = document.getElementById(id)
+			x.style.display = 'block';
 		}
 
-		function normalMOA() {
-			var x = document.getElementById("MOA");
-			x.style.height = "15vh";
-			x.style.width = "8vw";
-		}
-
-		function bigNitobe() {
-			var x = document.getElementById("Nitobe");
-			x.style.height = "25.2vh";
-			x.style.width = "13.2vw";
-		}
-
-		function normalNitobe() {
-			var x = document.getElementById("Nitobe");
-			x.style.height = "20.5vh";
-			x.style.width = "11vw";
-		}
-
-		function bigBeatty() {
-			var x = document.getElementById("Beatty");
-			x.style.height = "25.2vh";
-			x.style.width = "12vw";
-		}
-
-		function normalBeatty() {
-			var x = document.getElementById("Beatty");
-			x.style.height = "20.5vh";
-			x.style.width = "10vw";
-		}
-
-		function bigNest() {
-			var x = document.getElementById("Nest");
-			x.style.height = "18vh";
-			x.style.width = "10.08vw";
-		}
-
-		function normalNest() {
-			var x = document.getElementById("Nest");
-			x.style.height = "16vh";
-			x.style.width = "8.4vw";
-		}
-
-		function bigPacific() {
-			var x = document.getElementById("Pacific");
-			x.style.height = "13.2vh";
-			x.style.width = "7.2vw";
-		}
-
-		function normalPacific() {
-			var x = document.getElementById("Pacific");
-			x.style.height = "11vh";
-			x.style.width = "6vw";
-		}
-
-		function bigBus() {
-			var x = document.getElementById("Bus");
-			x.style.height = "14.4vh";
-			x.style.width = "13.2vw";
-		}
-
-		function normalBus() {
-			var x = document.getElementById("Bus");
-			x.style.height = "12vh";
-			x.style.width = "11vw";
+		function hideStuff(id) {
+			var x = document.getElementById(id);
+			x.style.display = 'none';
 		}
