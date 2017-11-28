ceda6bd969f775bef3f020d97cd6cb7a83013cce
lbingle <lebingle@gmail.com>
2017-11-27 16:59:04 -0800

    revised js to work with new image ids

--- js/mouse_functions.js
+++ js/mouse_functions.js
@@ -11,52 +11,62 @@
 			x.style.width = "8vw";
 		}
 
-		function bigNitobe(x) {
+		function bigNitobe() {
+			var x = document.getElementById("Nitobe");
 			x.style.height = "25.2vh";
 			x.style.width = "13.2vw";
 		}
 
-		function normalNitobe(x) {
+		function normalNitobe() {
+			var x = document.getElementById("Nitobe");
 			x.style.height = "20.5vh";
 			x.style.width = "11vw";
 		}
 
-		function bigBeatty(x) {
+		function bigBeatty() {
+			var x = document.getElementById("Beatty");
 			x.style.height = "25.2vh";
 			x.style.width = "12vw";
 		}
 
-		function normalBeatty(x) {
+		function normalBeatty() {
+			var x = document.getElementById("Beatty");
 			x.style.height = "20.5vh";
 			x.style.width = "10vw";
 		}
 
-		function bigNest(x) {
+		function bigNest() {
+			var x = document.getElementById("Nest");
 			x.style.height = "18vh";
 			x.style.width = "10.08vw";
 		}
 
-		function normalNest(x) {
+		function normalNest() {
+			var x = document.getElementById("Nest");
 			x.style.height = "16vh";
 			x.style.width = "8.4vw";
 		}
 
-		function bigPacific(x) {
+		function bigPacific() {
+			var x = document.getElementById("Pacific");
 			x.style.height = "13.2vh";
 			x.style.width = "7.2vw";
 		}
 
-		function normalPacific(x) {
+		function normalPacific() {
+			var x = document.getElementById("Pacific");
 			x.style.height = "11vh";
 			x.style.width = "6vw";
 		}
 
-		function bigBus(x) {
+		function bigBus() {
+			var x = document.getElementById("Bus");
 			x.style.height = "14.4vh";
 			x.style.width = "13.2vw";
 		}
 
-		function normalBus(x) {
+		function normalBus() {
+			var x = document.getElementById("Bus");
 			x.style.height = "12vh";
 			x.style.width = "11vw";
 		}
