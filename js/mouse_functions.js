c7857136c5ebdbee4088f11587bb8c613f7146b0
lbingle <lebingle@gmail.com>
2017-11-28 18:19:49 -0800

    Create JS to highlight Where are you buttons when selected
    Also hides sibling button in the Where are you going section

--- js/mouse_functions.js
+++ js/mouse_functions.js
@@ -9,7 +9,28 @@
 			x.style.display = 'none';
 		}
 
+//Functions when clicking on buttons
+		function borderRed(x, hide_thing){
+				x.className = 'red';
+				hideStuff(hide_thing);
+		}
+
+		function borderNone(x, hide_thing){
+				x.className ='none';
+				showStuff(hide_thing);
+		}
+
 		function border(id){
 			var x =document.getElementById(id);
-			x.style.borderColor = 'red';
+			//to remove button in, Where do you want to go?
+			var number_add = '2';
+			var hide_thing = id.concat('', number_add);
+			//action on initil click
+			if (x.classList.contains('none')){
+				borderRed(x, hide_thing);
+			}
+			//action on reclick
+			else {
+				borderNone(x, hide_thing);
+			}
 		}
