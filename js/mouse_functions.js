7161cb5142c7f2b245112822a62238d182f9cf53
lbingle <lebingle@gmail.com>
2017-11-30 20:03:36 -0800

    Show locations when button is selected

--- js/mouse_functions.js
+++ js/mouse_functions.js
@@ -1,6 +1,6 @@
 //mouseover and mouseoff functions
 		function showStuff(id) {
-			var x = document.getElementById(id)
+			var x = document.getElementById(id);
 			x.style.display = 'block';
 		}
 
@@ -9,7 +9,7 @@
 			x.style.display = 'none';
 		}
 
-//Functions when clicking on buttons
+//Functions when clicking on Where are you? buttons
 		function borderRed(x, hide_thing){
 				x.className = 'red';
 				hideStuff(hide_thing);
@@ -24,13 +24,17 @@
 			var x =document.getElementById(id);
 			//to remove button in, Where do you want to go?
 			var number_add = '2';
+			var number_add2= '3';
 			var hide_thing = id.concat('', number_add);
+			var show_thing = id.concat('', number_add2);
 			//action on initil click
 			if (x.classList.contains('none')){
 				borderRed(x, hide_thing);
+				showStuff(show_thing);
 			}
 			//action on reclick
 			else {
 				borderNone(x, hide_thing);
+				hideStuff(show_thing);
 			}
 		}
