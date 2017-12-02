2a792bf8db99753b9b6051e9c590c7e6c9425fce
lbingle <lebingle@gmail.com>
2017-12-01 18:21:55 -0800

    Notes on next steps

--- js/where-are-you-going.js
+++ js/where-are-you-going.js
@@ -3,10 +3,10 @@
 		function Going() {
 			var x = document.getElementsByClassName("red");
 			var num_red = x.length
-			document.getElementById("demo").innerHTML = num_red;
 			if (num_red == 0) {
 				window.alert('Where are you? Select location from the list above.');
 			} else if (num_red == 1) {
+				//Function to determine route to display
 				alert('Good');
 			} else if (num_red > 1) {
 				alert('Wow, you are in two places at once! Unless you are superhuman, I would suggest selecting one location.');
