634b0393963b306353c31a57dbc8900b5150f562
lbingle <lebingle@gmail.com>
2017-12-01 18:10:23 -0800

    warning messages about selecting starting point

---
+++ js/where-are-you-going.js
@@ -0,0 +1,14 @@
+//Functions when clicking on Where do you want to go?
+
+		function Going() {
+			var x = document.getElementsByClassName("red");
+			var num_red = x.length
+			document.getElementById("demo").innerHTML = num_red;
+			if (num_red == 0) {
+				window.alert('Where are you? Select location from list above.');
+			} else if (num_red == 1) {
+				alert('Good');
+			} else if (num_red > 1) {
+				alert('Wow, you are in two places at once! Unless you are superhuman, I would suggest selecting one location.');
+			}
+		}
