16cede9743feaedcca4002c18a9da39442a03b57
lbingle <lebingle@gmail.com>
2017-12-02 10:59:07 -0800

    showing path image

---
+++ js/test.js
@@ -0,0 +1,8 @@
+def hide_whereAre() {
+	var list_buttons = ['Be', 'Bu', 'MO', 'Ne', 'Ni', 'Pa'];
+	var x = document.getElementsByClassName("red");
+	if x = list_buttons[0]{
+		document.getElementById('Test-Path').style.display = 'block'
+	}
+}
+
--- js/where-are-you-going.js
+++ js/where-are-you-going.js
@@ -1,14 +1,20 @@
 //Functions when clicking on Where do you want to go?
-
-		function Going() {
-			var x = document.getElementsByClassName("red");
-			var num_red = x.length
-			if (num_red == 0) {
-				window.alert('Where are you? Select location from the list above.');
-			} else if (num_red == 1) {
-				//Function to determine route to display
-				alert('Good');
-			} else if (num_red > 1) {
-				alert('Wow, you are in two places at once! Unless you are superhuman, I would suggest selecting one location.');
-			}
+function showStuff(id) {
+			var x = document.getElementById(id);
+			x.style.display = 'block';
 		}
+		
+function Going() {
+	var x = document.getElementsByClassName("red");
+	var num_red = x.length
+	if (num_red == 0) {
+		window.alert('Where are you? Select location from the list above.');
+	} else if (num_red == 1) {
+	//Need way to determine route based on value of x
+	//This should end with an image name that can be placed into showStuff(id)
+	//Could name routes something like Be-Bu2 which would then coorespond to and image r1, r2, r3...
+		showStuff('Be3');
+	} else if (num_red > 1) {
+		alert('Wow, you are in two places at once! Unless you are superhuman, I would suggest selecting one location.');
+	}
+	}
