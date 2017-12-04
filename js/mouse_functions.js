0aca03cba657e5872855468e132eab4b515aa96c
lbingle <lebingle@gmail.com>
2017-12-03 16:46:51 -0800

    develop function to create path name to insert into show

--- js/mouse_functions.js
+++ js/mouse_functions.js
@@ -1,5 +1,5 @@
 //Branch test text
-//mouseover and mouseoff functions
+//functions to show and hide location highlighting
 		function showStuff(id) {
 			var x = document.getElementById(id);
 			x.style.display = 'block';
@@ -10,7 +10,7 @@
 			x.style.display = 'none';
 		}
 
-//Functions when clicking on Where are you? buttons
+//Functions to show and hide button highlighting
 		function borderRed(x, hide_thing){
 				x.className = 'red';
 				hideStuff(hide_thing);
@@ -21,6 +21,19 @@
 				showStuff(hide_thing);
 		}
 
+//Function to decide to turn highlighting on or off
+
+		function borderOnOff(x, hide_thing, show_thing){
+			if(x.classList.contains('none')){
+				borderRed(x, hide_thing);
+				showStuff(show_thing);
+			} else {
+				borderNone(x, hide_thing);
+				hideStuff(show_thing);
+			}
+		}
+
+//Function to use with Where are you? buttons
 		function border(id){
 			var x =document.getElementById(id);
 			//to remove button in, Where do you want to go?
@@ -28,14 +41,5 @@
 			var number_add2= '3';
 			var hide_thing = id.concat('', number_add);
 			var show_thing = id.concat('', number_add2);
-			//action on initil click
-			if (x.classList.contains('none')){
-				borderRed(x, hide_thing);
-				showStuff(show_thing);
-			}
-			//action on reclick
-			else {
-				borderNone(x, hide_thing);
-				hideStuff(show_thing);
-			}
+			borderOnOff(x, hide_thing, show_thing);
 		}
