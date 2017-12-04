2ee690a1df79a175e091ff08097a1270098d1b5e
lbingle <lebingle@gmail.com>
2017-12-03 21:17:39 -0800

    fitting image and setting up actions based on highlighting

--- js/mouse_functions.js
+++ js/mouse_functions.js
@@ -43,3 +43,51 @@
 			var show_thing = id.concat('', number_add2);
 			borderOnOff(x, hide_thing, show_thing);
 		}
+
+//Functions to use with Where are you going? buttons
+//using value property to return option value assigned to element
+function path(id) {
+	var x = document.getElementsByClassName("red");
+	var value_x = x[0].value;
+	var path = id.concat('-', value_x);
+	showStuff(path)
+	//alert(path)
+}
+
+function message(num_red, id) {
+	if (num_red == 0) {
+		window.alert('Where are you? Select location from the list above.');
+	} else if (num_red == 1) {
+		path(id);
+	} else if (num_red > 1) {
+		alert('Wow, you are in two places at once! Unless you are superhuman, I would suggest selecting one location.');
+	}
+}
+
+function goingHighlight(y, show_thing) {
+	if(y.classList.contains('none')){
+		showStuff(show_thing);
+		y.className = 'green'
+	} else {
+		hideStuff(show_thing);
+		y.className ='none';
+	}
+}
+
+function Going(id) {
+	var x = document.getElementsByClassName("red");
+	var y = document.getElementById(id)
+	var z = document.getElementsByClassName('green')
+	var id_base = id.substring(0, 2);
+	var show_thing = id_base.concat('3')
+	var num_red = x.length;
+	var num_green = z.length;
+	goingHighlight(y, show_thing);
+	message(num_red, id);
+	}
+
+function test(id) {
+	x = id.substring(0, 2);
+	y = x.concat('3')
+	alert(y);
+}
