2762e2c45f4f8f7e77e226908184b241cfeb3f87
lbingle <lebingle@gmail.com>
2017-12-11 21:41:13 -0800

    factoring code

--- js/mouse_functions.js
+++ js/mouse_functions.js
@@ -1,4 +1,4 @@
-//functions to show and hide location highlighting
+//functions to show and hide elements
 		function showStuff(id) {
 			var x = document.getElementById(id);
 			x.style.display = 'block';
@@ -9,34 +9,17 @@
 			x.style.display = 'none';
 		}
 
-//Functions to show and hide button highlighting
-		function borderRed(x, hide_thing){
-				x.className = 'red';
+//Functions to change border color
+		function borderColor(x, color) {
+			x.className = color;
 		}
 
-		function borderNone(x, hide_thing){
-				x.className ='none';
-		}
-
-//Function to decide to turn highlighting on or off
-
-		function RedBorderOnOff(x, hide_thing, show_thing, id){
-			if(x.classList.contains('none')){
-				borderRed(x, hide_thing);
-				showStuff(show_thing);
-				hideStuff(hide_thing);
-				HideLocations(id)
-			} else {
-				borderNone(x, hide_thing);
-				hideStuff(show_thing);
-				showStuff(hide_thing);
-				ShowLocations();
-			}
+		function borderNone(x) {
+			x.className ='none';
 		}
 
 //Function to hide Location buttons that are not selected
-		function HideLocations(id) {
-			loc = ['Be','Bu','MO','Ne','Ni','Pa'];
+		function HideButtons(id, loc) {
 			var loc_length = loc.length;
 			var i;
 			for (i=0; i < loc_length; i++){
@@ -47,92 +30,66 @@
 				}
 		}
 
-		function ShowLocations() {
-			loc = ['Be','Bu','MO','Ne','Ni','Pa'];
+		function ShowButtons(loc, value_y_hide) {
 			var loc_length = loc.length;
 			var i;
 			for (i=0; i < loc_length; i++){
-					var array_id = loc[i];
-					showStuff(array_id);
-				}
-		}		
+				var array_id = loc[i];
+				showStuff(array_id);
+			}
+		}
 
 //Function to use with Where are you? buttons
-		function border(id){
-			var x =document.getElementById(id);
-			//to remove button in, Where do you want to go?
-			var number_add = '2';
-			var number_add2= '3';
-			var hide_thing = id.concat('', number_add);
-			var show_thing = id.concat('', number_add2);
-			RedBorderOnOff(x, hide_thing, show_thing, id);
-		}
-
-//Functions to use with Where are you going? buttons
-//using value property to return option value assigned to element
-
-//Function to hide and show Destination buttons that are not selected
-function HideDestination(id) {
-	loc = ['Be2','Bu2','MO2','Ne2','Ni2','Pa2'];
-	var loc_length = loc.length;
-	var i;
-	for (i=0; i < loc_length; i++){
-		var array_id = loc[i];
-		if(id != array_id) {
-		hideStuff(array_id);
-		}
-	}
-}
-
-function ShowDestination() {
-	loc = ['Be2','Bu2','MO2','Ne2','Ni2','Pa2'];
-	var loc_length = loc.length;
-	var i;
-	for (i=0; i < loc_length; i++){
-		var array_id = loc[i];
-		showStuff(array_id);
-	}
-}
-
-function path(id, x) {
-	var value_x = x[0].value;
-	var path = id.concat('-', value_x);
-	showStuff(path);
-}
-
-function pathHide(id, x) {
-	var value_x = x[0].value;
-	var path = id.concat('-', value_x);
-	hideStuff(path);
-}
-
-function borderGreen(y) {
-	y.className = 'green';
-}
-
-function borderNoneG(y) {
-	y.className ='none';
-}
-
-
-function GreenBorderOnOff(x, y, show_thing, id) {
-	if(y.classList.contains('none')){
-		borderGreen(y);
+function border(id){
+	var x =document.getElementById(id);
+	var y = document.getElementsByClassName("red");
+	var z = document.getElementsByClassName("green");
+	var hide_thing = id.concat('2');
+	var show_thing = id.concat('3');
+	//var value_y = y[0].value;
+	//var value_y_hide = value_y.substring(0,2);
+	var loc = ['Be','Bu','MO','Ne','Ni','Pa'];
+	if(x.classList.contains('none')){
+		borderColor(x, 'red');
 		showStuff(show_thing);
-		HideDestination(id);
-		path(id, x);
+		hideStuff(hide_thing);
+		HideButtons(id, loc)
 	} else {
-		borderNoneG(y);
+		borderNone(x);
 		hideStuff(show_thing);
-		ShowDestination();
-		pathHide(id, x);
+		showStuff(hide_thing);
+		ShowButtons(loc, value_y_hide);
 	}
 }
 
+//Function to use with Where are you going? buttons
 function Going(id) {
-	var x = document.getElementsByClassName("red");
-	var y = document.getElementById(id)
+	var x = document.getElementById(id);
+	var y = document.getElementsByClassName("red");
+	var z = document.getElementsByClassName("green")
 	var id_base = id.substring(0, 2);
-	var show_thing = id_base.concat('3')
-	GreenBorderOnOff(x, y, show_thing, id);
+	var show_thing = id_base.concat('3');
+	var loc = ['Be2','Bu2','MO2','Ne2','Ni2','Pa2'];
+	var value_y = y[0].value;
+	var value_y_base = value_y.substring(0,2);
+	var value_y_hide = value_y_base.concat('2');
+	var path_id = id.concat('-', value_y);
+
+	if(x.classList.contains('none')){
+		borderColor(x, 'green');
+		showStuff(show_thing);
+		HideButtons(id, loc);
+		showStuff(path_id);
+	} else {
+		borderNone(x);
+		hideStuff(show_thing);
+		hideStuff(path_id);
+		ShowButtons(loc, value_y_hide);
 	}
+	}
+
+//Test functions using test button, results displayed in a pop up
+function test() {
+	var x = document.getElementsByClassName("red");
+	alert(value_x_hide)
+}
